76a31e7014560ec57ef681ed6e2c3975a53e976e
saidurga@1996 <saidurgasai96@gmail.com>
2019-05-30 04:39:34 -0700

    hi
    Signed-off-by: saidurga@1996 <saidurgasai96@gmail.com>

---
+++ DemoGit/src/TestEmployee.java
@@ -0,0 +1,11 @@
+
+public class TestEmployee {
+
+	public static void main(String[] args) {
+		Employee e1=new Employee();
+		e1.setEmpId(101);
+        e1.setEmpName("sai");
+        e1.setEmpSal(1000);
+	}
+
+}
